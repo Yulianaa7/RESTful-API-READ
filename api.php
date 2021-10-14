fa7710feb327dad6a3f5769b2ad7a9013da78b8b
Yulianaa7 <yuliananurhanifah77@gmail.com>
2021-10-14 20:31:02 +0700

    Menambah Fungsi Read

---
+++ api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/pelanggan', 'PelangganController@show');
+Route::post('/pelanggan', 'PelangganController@store');
+
+Route::get('/produk', 'ProdukController@show');
+Route::post('/produk', 'ProdukController@store');
+
+Route::get('/transaksi', 'TransaksiController@show');
+Route::get('/transaksi/{id_transaksi}', 'TransaksiController@detail');
+Route::post('/transaksi', 'TransaksiController@store');
+
+Route::get('/detail_transaksi', 'Detail_TransaksiController@show');
+Route::get('/detail_transaksi/{id_detail_transaksi}', 'Detail_TransaksiController@detail');
+Route::post('/detail_transaksi', 'Detail_TransaksiController@store');
